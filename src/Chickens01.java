971687acabdccd6a7e469a68e893d08079fb42ce
PHarryP <113381182+PHarryP@users.noreply.github.com>
2022-09-21 14:37:30 -0400

    chickens1

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -1,7 +1,10 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
-        
-        System.out.println(totalEggs);
+     int totalEggs = 0;
+     int eggsPerChicken = 5;
+     int chickenCount = 3;		 
+     
+     System.out.println(totalEggs);
     }   
 }
